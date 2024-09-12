f3efde3c32a44f033503e3ae128819440f09a19e
PauloHBSF <paulohbsfilho@gmail.com>
2024-09-12 12:37:36 -0300

    finished routes

---
+++ backend/routes.py
@@ -0,0 +1,52 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import List
+
+from backend.database import SessionLocal, get_db
+from backend.schemas import ProductRead, ProductUpdate, ProductCreate
+from backend.crud import (
+    create_product,
+    get_products,
+    get_product,
+    delete_product,
+    update_product
+)
+
+router = APIRouter()
+
+
+@router.get('/products/', response_model=List[ProductRead])
+def read_all_products(db: Session = Depends(get_db)):
+    products = get_products(db)
+    return products
+
+
+@router.get('/product/{product_id}', response_model=ProductRead)
+def read_one_product(product_id: int, db: Session = Depends(get_db)):
+    product = get_product(db, product_id=product_id)
+
+    if product is None:
+        raise HTTPException(status_code=404, detail="Produto não encontrado.")
+
+    return product
+
+
+@router.post('/products/', response_model=ProductRead)
+def create_product(product: ProductCreate, db: Session = Depends(get_db)):
+    new_product = create_product(db, product=product)
+    return new_product
+
+
+@router.delete('/product/{product_id}', response_model=ProductRead)
+def delete_product(product_id: int, db: Session = Depends(get_db)):
+    deleted_product = delete_product(db, product_id=product_id)
+    if deleted_product is None:
+        raise HTTPException(status_code=404, detail="Produto não encontrado.")
+    return deleted_product
+
+@router.put('/product/{product_id}', response_model=ProductRead)
+def create_product(product_id: int, product:ProductUpdate, db: Session = Depends(get_db)):
+    added_product = update_product(db, product_id=product_id, product=product)
+    if added_product is None:
+        raise HTTPException(status_code=404, detail="Produto não encontrado.")
+    return added_product
